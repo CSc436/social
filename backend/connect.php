65deb8dce8c36a043ba35999cd0aaadf0ff22626
btidwell <btidwell@email.arizona.edu>
2014-03-04 22:35:57 -0700

    created the PHP script process_create_account.php which takes user information provided via form POST, checks that the input is valid, and inserts into the DB

--- backend/connect.php
+++ backend/connect.php
@@ -5,4 +5,7 @@
         "crowdsocial",
         "pooreview"
     );
+	
+	// Have the PDO object throw an exception if anything happens.
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
