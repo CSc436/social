b5c9a8021751f11b89689164a8d8edcf5dde65dd
btidwell <btidwell@email.arizona.edu>
2014-03-05 17:21:35 -0700

    Created a UI for creating a new account. Not yet integrated with the sidebar

--- backend/connect.php
+++ backend/connect.php
@@ -1,10 +1,10 @@
 <?php
-	// Create a new PDO object.
+	// Create a new PDO object
 	$db = new PDO(
-        "mysql:host=localhost;dbname=cstestdb",
-        "crowdsocial",
-        "pooreview"
-    );
+		"mysql:host=localhost;dbname=cstestdb",
+		"crowdsocial",
+		"pooreview"
+	);
 	
 	// Have the PDO object throw an exception if anything happens.
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
