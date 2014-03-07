6e67e03dc93187826cd14c09d4df672307d1a967
rbaldwin1 <rbaldwin@email.arizona.edu>
2014-03-06 19:12:28 -0700

    Updated css

--- Front End/social.js
+++ Front End/social.js
@@ -3,19 +3,15 @@ $(document).ready(function () {
     $("#showhide").click(function () {
         $("#sidebar-main").toggle("slide",{direction: "left"},500);
         $(this).toggle("slide",{direction: "left"},500);
-<<<<<<< HEAD
         if ($("#event-list").is(":visible")) {
             $("#event-list").hide();
         }    
-=======
->>>>>>> 29296851537bb7a8d0438900b55a08086a50a01b
     });
     
     $("#showhide-hidden").click(function () {
         $("#sidebar-main").toggle("slide",{direction: "left"},500);
         $("#showhide").toggle("slide",{direction: "left"},500);
     });    
-<<<<<<< HEAD
 });
 
 //slide for event list
@@ -26,7 +22,4 @@ $(document).ready(function () {
     $("#events-button").click(function () {
         eventList.toggle("slide",{direction: "left"}, 500);
     });    
-});    
-=======
 });
->>>>>>> 29296851537bb7a8d0438900b55a08086a50a01b
