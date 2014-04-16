a91a583a4c6832798b0cf8dd009ac1bdad39399a
btidwell <btidwell@email.arizona.edu>
2014-04-15 23:39:58 -0700

    fixed up getEvents.php and added some security fuctionality

--- Front End/getEvents.php
+++ Front End/getEvents.php
@@ -2,32 +2,41 @@
  	include("connect.php");
 
  	$queryString = "
-		Select * From event, locale,category
+		Select * From event, locale, category
 		where event.locationid=locale.locationid 
 		AND event.categoryID=category.categoryID
-		AND ABS(".$_GET['currentLat']."-latitude) < (3276.8/POW(2,".$_GET['zoom']."))
-		AND ABS(".$_GET['currentLong']."-longitude) < (3276.8/POW(2,".$_GET['zoom'].")"
+		AND ABS(:currentLat-latitude) < (3276.8/POW(2,:zoom))
+		AND ABS(:currentLong-longitude) < (3276.8/POW(2,:zoom))";
+		
+	$params = array(
+		":currentLat" => htmlspecialchars($_GET['currentLat']),
+		":currentLong" => htmlspecialchars($_GET['currentLong']),
+		":zoom" => htmlspecialchars($_GET['zoom']),
+	);
 
+	// ***** APPLY FILTERS *****
 	if(isset($_GET['Category'])){
-		$queryString.="AND category.categoryName='".$_GET['Category']."'"
+		$params[":category"] = htmlspecialchars($_GET['Category']);
+		$queryString.=" AND category.CategoryName=':category'";
 	}
 	if(isset($_GET['Title'])){
-		$queryString.="AND event.Title LIKE '%".$_GET['Title']."%'"
+		$params[":title"] = htmlspecialchars($_GET['Title']);
+		$queryString.=" AND event.Title LIKE '%:title%'";
 	}
 	if(isset($_GET['Owner'])){
-		$queryString.="AND event.Email='".$_GET['Owner']."'"
+		$params[":owner"] = htmlspecialchars($_GET['Owner']);
+		$queryString.=" AND event.Email=':owner'";
 	}
 	if(isset($_GET['StartDate'])){
-		//$queryString.="AND category.categoryName='".$_GET['Category']."'"
+		//$params[":startdate"] = htmlspecialchars($_GET['StartDate']);
+		//$queryString.=" AND event.StartDate=':startdate'";
 	}
-	$queryString.=");"
+	
+	$queryString.=";";
 	$query = $db->prepare($queryString);
 	
 	// Execute the query.
-	$query->execute();
-	
-	// Fetch every result.
-	//$result = $query->fetchAll();
+	$query->execute($params);
 
     echo json_encode($query->fetchAll());
 ?>
