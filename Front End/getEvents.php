217eb66242b5c80b2dc7e42fbd41797c959e3f23
jtklier <jtklier@email.arizona.edu>
2014-04-15 21:07:59 -0700

    Added PHP code for filtering purposes
    if the variables passed through the $_GET array are set, the
    corresponding filter conditions will be added to the db query

--- Front End/getEvents.php
+++ Front End/getEvents.php
@@ -1,13 +1,27 @@
 <?php
  	include("connect.php");
 
-	$query = $db->prepare("
+ 	$queryString = "
 		Select * From event, locale,category
 		where event.locationid=locale.locationid 
 		AND event.categoryID=category.categoryID
 		AND ABS(".$_GET['currentLat']."-latitude) < (3276.8/POW(2,".$_GET['zoom']."))
-		AND ABS(".$_GET['currentLong']."-longitude) < (3276.8/POW(2,".$_GET['zoom']."));
-	");
+		AND ABS(".$_GET['currentLong']."-longitude) < (3276.8/POW(2,".$_GET['zoom'].")"
+
+	if(isset($_GET['Category'])){
+		$queryString.="AND category.categoryName='".$_GET['Category']."'"
+	}
+	if(isset($_GET['Title'])){
+		$queryString.="AND event.Title LIKE '%".$_GET['Title']."%'"
+	}
+	if(isset($_GET['Owner'])){
+		$queryString.="AND event.Email='".$_GET['Owner']."'"
+	}
+	if(isset($_GET['StartDate'])){
+		//$queryString.="AND category.categoryName='".$_GET['Category']."'"
+	}
+	$queryString.=");"
+	$query = $db->prepare($queryString);
 	
 	// Execute the query.
 	$query->execute();
