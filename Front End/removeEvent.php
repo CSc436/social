54980d1076a254eb7afe4388b7b099d4a5ef34b4
btidwell <btidwell@email.arizona.edu>
2014-05-06 21:24:29 -0700

    adjusted how events are removed;

--- Front End/removeEvent.php
+++ Front End/removeEvent.php
@@ -2,7 +2,9 @@
 	include '..\backend\connect.php';
 
 	$event_query = $db->prepare("
+		DELETE FROM notifications WHERE EventId=:eventid;
 		DELETE FROM eventkeyword WHERE EventId=:eventid;
+		DELETE FROM attending WHERE Event=:eventid;
 		DELETE FROM event WHERE EventId=:eventid;
 	");
 	
